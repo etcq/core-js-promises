390b7628e206ee143d7b6554ce9fa2e73d78498f
Anton Sushnikov <resonans93@gmail.com>
2025-04-09 00:11:20 +0500

    6/7

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -81,8 +81,8 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return Promise.race(promises);
 }
 
 /**
@@ -97,10 +97,7 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
 function getAllOrNothing(promises) {
-  return Promise.all(promises).then(
-    (value) => value,
-    (reason) => reason
-  );
+  return Promise.all(promises);
 }
 
 /**
@@ -115,8 +112,12 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((values) => {
+    return values.map((item) =>
+      item.status === 'fulfilled' ? item.value : null
+    );
+  });
 }
 
 /**
@@ -137,8 +138,13 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  let string = '';
+  promises.forEach((prom) => {
+    string += prom;
+  });
+
+  return string;
 }
 
 module.exports = {
