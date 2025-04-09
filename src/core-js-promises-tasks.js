059af98c89b781ab3b4fa6f98728e4b0f2c2eb0e
Anton Sushnikov <resonans93@gmail.com>
2025-04-09 13:59:32 +0500

    chance for 7/7

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -138,13 +138,15 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(promises) {
+async function queuePromises(promises) {
   let string = '';
-  promises.forEach((prom) => {
-    string += prom;
+  await promises.forEach((prom) => {
+    prom.then((val) => {
+      string += val;
+      return val;
+    });
   });
-
-  return string;
+  return Promise.resolve(string.toString());
 }
 
 module.exports = {
